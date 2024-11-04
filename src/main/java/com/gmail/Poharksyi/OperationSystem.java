5398c313d4a56b1a01e684b6cda160ee1bfa7460
Poharskyi <avpog.box@gmail.com>
2024-11-04 22:37:38 +0100

    add class and test

---
+++ src/main/java/com/gmail/Poharksyi/OperationSystem.java
@@ -0,0 +1,18 @@
+package com.gmail.Poharksyi;
+
+public enum OperationSystems {
+    WINDOWS_LEGACY,
+    WINDOWS10,
+    WINDOWS11,
+    MACOS,
+    UBUNTU,
+    FEDORA,
+    MINT,
+    KALI,
+    DEBIAN,
+    LINUX_OTHER,
+    UNKOWNOS;
+
+
+
+}
---
+++ src/main/java/com/gmail/Poharksyi/PcSetup.java
@@ -0,0 +1,9 @@
+package com.gmail.Poharksyi;
+
+public class PcProperties {
+    private int ramAmount;
+    private int coreAmount;
+    private String name;
+    private String uuid;
+    
+}

f821ff7b58214c4f92969c2dd7447802cfe326cb
Poharskyi <avpog.box@gmail.com>
2024-11-07 18:19:56 +0100

    fix and cleaning

--- src/main/java/com/gmail/Poharksyi/App.java
+++ src/main/java/com/gmail/Poharksyi/App.java
@@ -8,7 +8,7 @@ public class App
     {
         PcSetup myPcSetup = new PcSetup(64, 8, "Firefly", "76562335-4D9A-481D-A8CA-2CF05D556F4C", WINDOWS11);
         PcSetup liohaPcSetup = new PcSetup(32, 8, "LiohaPC", "76562335-4D9A-481D-A8CA-2CF05KJHSSd", MACOS);
-        PcSetup myPcSetupClone = new PcSetup(64, 8, "Firefly", "76562335-4D9A-481D-A8CA-2CFSDAW56F4C", WINDOWS11);
+        PcSetup myPcSetupClone = new PcSetup(64, 8, "Firefly", "76562335-4D9A-481D-A8CA-2CF05D556F4C", WINDOWS11);
         PcSetup valeriaPcSetup = new PcSetup(64, 4, "LiohaPC", "76562335-4D9A-481D-A8CA-2CFASDWFEA", DEBIAN);
 
         System.out.println(myPcSetup.equals(liohaPcSetup));
@@ -27,6 +27,10 @@ public class App
 
         System.out.println();
 
-        System.out.println(myPcSetup.toString());
+        System.out.println(myPcSetup.hashCode() == valeriaPcSetup.hashCode());
+
+        System.out.println();
+
+        //System.out.println(myPcSetup.toString());
     }
 }
